0728ee33f011d7c319038b1fc8726f6fbd42c7c2
Marvin Minhaz <marvinminhaz@outlook.com>
2025-12-04 16:54:09 +0530

    Fixed the first task replacement bug

--- script.js
+++ script.js
@@ -1,44 +1,74 @@
-//Selecting elements
+
+// Selecting elements
 
 const input = document.querySelector('.input-group input');
 const addBtn = document.querySelector('.add-btn');
 const p = document.getElementById('p1')
 const listGroup = document.querySelector('.list-group');
 
-//Function to add task
+
+//counting tasks
+let count = 0;
+
+
+
+//function to add task 
 function addTask() {
     const taskText = input.value.trim();
-    console.log(taskText);
 
-    //dont add empty tasks
-    if (taskText === '') {
-        return
+    if (taskText !== ''){
+        count++;
     }
-
-    //creating elements
-    const taskItem = document.createElement('label');
-    taskItem.className = 'task-item';
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    const taskParagraph = document.createElement('p');
-    taskParagraph.textContent = taskText;
-
-    //appending
-    taskItem.appendChild(checkbox);
-    taskItem.appendChild(taskParagraph);
-    listGroup.appendChild(taskItem);
-
-    // document.getElementById('p1').innerHTML = taskText; doesnt work bc creates new element and also changes the first line
-
-    //clearing input
-    input.value = '';
-
-    //focus back on input
-    input.focus();
+    
+    console.log(count);
 
     
 
+
+    //conditional to replace the "add your first task" statement on addition of first task and creating new elements after the first task
+    if (count == 1) {
+
+        // dont add empty tasks
+        if (taskText === '') {
+            return
+        }
+        //replacing the "add your first task" statement with input
+        document.getElementById('p1').innerHTML = taskText;
+
+        //clearing input
+        input.value = '';
+
+        //focus back on input
+        input.focus()
+    } else {
+
+        // dont add empty tasks
+        if (taskText === '') {
+            return
+        }
+
+        //creating elements
+        const taskItem = document.createElement('label');
+        taskItem.className = 'task-item';
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        const taskParagraph = document.createElement('p');
+        taskParagraph.textContent = taskText;
+
+        //appending
+        taskItem.appendChild(checkbox);
+        taskItem.appendChild(taskParagraph);
+        listGroup.appendChild(taskItem);
+
+        //clearing input
+        input.value = '';
+
+        //focus back on input
+        input.focus()
+    }
+
 }
+
 
 //function to addTask on Enter
 function eol(e) {
@@ -51,15 +81,3 @@ function eol(e) {
 //event listening click and enter
 addBtn.addEventListener('click', addTask);
 input.addEventListener('keydown', eol);
-
-//reference for addEventListener (e)
-// input.addEventListener('keydown', (e) => {
-//   // e is the event object, it has lots of info:
-//   console.log(e.key);        // Which key? 'Enter', 'a', 'Shift', etc.
-//   console.log(e.type);       // What event? 'keydown'
-//   console.log(e.target);     // Which element? The input element
-//   console.log(e.timeStamp);  // When? Timestamp in milliseconds
-// });
-
-
-
